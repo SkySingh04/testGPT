cab32c08afb7005fa7659659e2253e11a2dcbfa8
Sky Singh <akashsingh2210670@gmail.com>
2025-03-16 00:19:31 +0530

    refactor: streamline script permission grants and enhance result parsing logic
    - Combined multiple chmod commands into a single line for efficiency in action.yml.
    - Added a check in parse-results.sh to handle cases where no tests were executed, ensuring proper output generation.
    
    Signed-off-by: Sky Singh <akashsingh2210670@gmail.com>

--- parse-results.sh
+++ parse-results.sh
@@ -4,7 +4,12 @@ grep -oE "COMPLETE TESTRUN SUMMARY\.\s+Total tests: [0-9]+" ${GITHUB_WORKSPACE}/
 grep -oE "COMPLETE TESTRUN SUMMARY\.\s+Total test passed: [0-9]+" ${GITHUB_WORKSPACE}/${WORKDIR}/report.txt | sed -r "s/\x1B\[[0-9;]*[mGK]//g" > ${GITHUB_WORKSPACE}/${WORKDIR}/final_total_passed.out
 grep -oE "COMPLETE TESTRUN SUMMARY\.\s+Total test failed: [0-9]+" ${GITHUB_WORKSPACE}/${WORKDIR}/report.txt | sed -r "s/\x1B\[[0-9;]*[mGK]//g" > ${GITHUB_WORKSPACE}/${WORKDIR}/final_total_failed.out
 
-cat ${GITHUB_WORKSPACE}/${WORKDIR}/final_total_tests.out ${GITHUB_WORKSPACE}/${WORKDIR}/final_total_passed.out ${GITHUB_WORKSPACE}/${WORKDIR}/final_total_failed.out > ${GITHUB_WORKSPACE}/${WORKDIR}/final.out
+# Check if any test results were found
+if [ ! -s "${GITHUB_WORKSPACE}/${WORKDIR}/final_total_tests.out" ]; then
+    echo "No tests were executed" > ${GITHUB_WORKSPACE}/${WORKDIR}/final.out
+else
+    cat ${GITHUB_WORKSPACE}/${WORKDIR}/final_total_tests.out ${GITHUB_WORKSPACE}/${WORKDIR}/final_total_passed.out ${GITHUB_WORKSPACE}/${WORKDIR}/final_total_failed.out > ${GITHUB_WORKSPACE}/${WORKDIR}/final.out
+fi
 
 echo 'KEPLOY_REPORT<<EOF' >> $GITHUB_OUTPUT
 cat ${GITHUB_WORKSPACE}/${WORKDIR}/final.out >> $GITHUB_OUTPUT
